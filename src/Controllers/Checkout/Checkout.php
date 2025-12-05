a01e3d0457ba45c45bd5efef00448e95d92d85af
Orlando Betancourth <orlando@uledger.io>
2025-12-04 20:59:49 -0600

    Paypal update to api

--- src/Controllers/Checkout/Checkout.php
+++ src/Controllers/Checkout/Checkout.php
@@ -4,25 +4,37 @@ namespace Controllers\Checkout;
 
 use Controllers\PublicController;
 
-class Checkout extends PublicController{
-    public function run():void
+class Checkout extends PublicController
+{
+    public function run(): void
     {
         $viewData = array();
         if ($this->isPostBack()) {
             $PayPalOrder = new \Utilities\Paypal\PayPalOrder(
-                "test".(time() - 10000000),
-                "http://localhost/mvco/index.php?page=checkout_error",
-                "http://localhost/mvco/index.php?page=checkout_accept"
+                "test" . (time() - 10000000),
+                "http://localhost:8080/mvc202402/index.php?page=Checkout_Error",
+                "http://localhost:8080/mvc202402/index.php?page=Checkout_Accept"
             );
+
             $PayPalOrder->addItem("Test", "TestItem1", "PRD1", 100, 15, 1, "DIGITAL_GOODS");
             $PayPalOrder->addItem("Test 2", "TestItem2", "PRD2", 50, 7.5, 2, "DIGITAL_GOODS");
-            $response = $PayPalOrder->createOrder();
-            $_SESSION["orderid"] = $response[1]->result->id;
-            \Utilities\Site::redirectTo($response[0]->href);
+
+            $PayPalRestApi = new \Utilities\PayPal\PayPalRestApi(
+                \Utilities\Context::getContextByKey("PAYPAL_CLIENT_ID"),
+                \Utilities\Context::getContextByKey("PAYPAL_CLIENT_SECRET")
+            );
+            $PayPalRestApi->getAccessToken();
+            $response = $PayPalRestApi->createOrder($PayPalOrder);
+
+            $_SESSION["orderid"] = $response->id;
+            foreach ($response->links as $link) {
+                if ($link->rel == "approve") {
+                    \Utilities\Site::redirectTo($link->href);
+                }
+            }
             die();
         }
 
         \Views\Renderer::render("paypal/checkout", $viewData);
     }
 }
-?>
